5d75a74dfb56013e4c6397993c9123f1569460f3
YuukiTS01 <lenamthaisonts@gmail.com>
2023-03-20 10:20:11 +0700

    first push

---
+++ app.js
@@ -0,0 +1,62 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const { config } = require('process');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// setup listen port
+var port = 3000
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
+
+// configure mysql connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "ThaiSon-2001",
+  database: "healthcare"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connection established")
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
