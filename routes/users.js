f3249eac990fe919010a6ea46300e75f1a6097f9
YuukiTS01 <lenamthaisonts@gmail.com>
2023-03-22 09:52:50 +0700

    adding modules

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,7 @@ var connection = require('../models/dbconfig')
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    var fields = "usr.*";
-    var source = "users usr";
-    var query = "select " + fields + " from "
-        + source;
+    var query = "select * from users";
     connection.query(query, function(err, result) {
         if (err) throw err;
         res.send(result);
@@ -15,15 +12,29 @@ router.get('/', function(req, res) {
 });
 
 router.get('/:userid', function(req, res) {
-    var fields = "usr.*";
-    var source = "users usr"
-    var condition = "usr.id = ?";
-    var query = "select " + fields + " from "
-        + source + " where " + condition;
+    var query = "call sp_view_profile(?)"
     connection.query(query, req.params.userid, function(err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
+
+router.post('/register', function(req, res) {
+    var query = "call sp_register(?, ?, ?, ?)"
+    var params = [req.body.username, req.body.password, req.body.email, req.body.phone];
+    connection.query(query, params, function(err, result) {
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+router.post('/:userid/update', function(req, res) {
+    var query = "call sp_update_profile(?, ?, ?, ?)"
+    var params = [req.params.userid, req.body.username, req.body.email, req.body.phone];
+    connection.query(query, params, function(err, result) {
+        if(err) throw err;
+        res.send(result);
+    })
+})
 
 module.exports = router;
