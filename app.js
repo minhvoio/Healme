f80d193fe4a1054e85a8d56e6218a9e2b01799c1
minhvo0o <vdminh@penphy.edu.vn>
2023-04-02 18:28:31 +0700

    Login with Role

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var clinicRouter = require('./routes/clinic');
-var pharmacyRouter = require('./routes/pharmacy');
-var branchRouter = require('./routes/branch');
-const { config } = require('process');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var clinicRouter = require("./routes/clinic");
+var pharmacyRouter = require("./routes/pharmacy");
+var branchRouter = require("./routes/branch");
+const { config } = require("process");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const port = process.env.PORT || 8080;
 
-app.use(logger('dev'));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/clinic', clinicRouter);
-app.use('/pharmacy', pharmacyRouter);
-app.use('/branch', branchRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/clinic", clinicRouter);
+app.use("/pharmacy", pharmacyRouter);
+app.use("/branch", branchRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,115 +1,133 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { signupValidation, loginValidation } = require('../models/validation');
-const { validationResult } = require('express-validator');
+const { signupValidation, loginValidation } = require("../models/validation");
+const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-var connection = require('../models/dbconfig')
+var connection = require("../models/dbconfig");
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-    var query = "select * from users";
-    connection.query(query, function(err, result) {
-        if (err) throw err;
-        res.send(result);
-    });
+router.get("/", function (req, res) {
+  var query = "select * from users";
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
-router.get('/:userid', function(req, res) {
-    var query = "call sp_view_profile(?)"
-    connection.query(query, req.params.userid, function(err, result) {
-        if (err) throw err;
-        res.send(result);
-    });
+router.get("/:userid", function (req, res) {
+  var query = "call sp_view_profile(?)";
+  connection.query(query, req.params.userid, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
-router.post('/register', function(req, res) {
-    var query = "call sp_register(?, ?, ?, ?)"
-    var params = [req.body.username, req.body.password, req.body.email, req.body.phone];
-    connection.query(query, params, function(err, result) {
-        if(err) throw err;
-        res.send(result);
-    })
-})
+router.post("/register", function (req, res) {
+  var query = "call sp_register(?, ?, ?, ?)";
+  var params = [
+    req.body.username,
+    req.body.password,
+    req.body.email,
+    req.body.phone,
+  ];
+  connection.query(query, params, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
-router.post('/:userid/update', function(req, res) {
-    var query = "call sp_update_profile(?, ?, ?, ?)"
-    var params = [req.params.userid, req.body.username, req.body.email, req.body.phone];
-    connection.query(query, params, function(err, result) {
-        if(err) throw err;
-        res.send(result);
-    })
-})
+router.post("/:userid/update", function (req, res) {
+  var query = "call sp_update_profile(?, ?, ?, ?)";
+  var params = [
+    req.params.userid,
+    req.body.username,
+    req.body.email,
+    req.body.phone,
+  ];
+  connection.query(query, params, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
-router.post('/api/login', loginValidation, (req, res) => {
+router.post("/api/login", loginValidation, (req, res) => {
   var params = [req.body.email];
-    var query = 'SELECT * FROM users WHERE email = ?'
-    var params = [req.body.email, req.body.password];
-    connection.query(query, params, function(err, result){
-        if(err) {
-          // throw err;
-          return res.status(400).send({
-            msg: err,
-          });
-        }
-        if (!result.length) {
-          return res.status(401).send({
-            msg: "Email or password is incorrect!",
-          });
-        }
-        // check password
-        bcrypt.compare(
-        req.body.password,
-        result[0]["pass"], (bErr, bResult) => {
-          if (bErr) {
-            // throw bErr;
-            return res.status(401).send({
-                msg: "Email or password is incorrect!",
-            });
-          }
-          if (bResult) {
-            const token = jwt.sign(
-                { id: result[0].id },
-                "the-super-strong-secrect",
-                { expiresIn: "1h" }
-            );
-            connection.query(
-                `UPDATE users SET last_login = now() WHERE id = '${result[0].id}'`
-            );
-            return res.status(200).send({
+  var query = "SELECT * FROM users WHERE email = ?";
+  var params = [req.body.email, req.body.password];
+  connection.query(query, params, function (err, result) {
+    if (err) {
+      // throw err;
+      return res.status(400).send({
+        msg: err,
+      });
+    }
+    if (!result.length) {
+      return res.status(401).send({
+        msg: "Email or password is incorrect!",
+      });
+    }
+    // check password
+    bcrypt.compare(req.body.password, result[0]["pass"], (bErr, bResult) => {
+      if (bErr) {
+        // throw bErr;
+        return res.status(401).send({
+          msg: "Email or password is incorrect!",
+        });
+      }
+      connection.query(
+        "SELECT * FROM roles WHERE id = ?",
+        result[0].role_id,
+        (roleErr, roleRes) => {
+          if (roleErr) throw roleErr;
+          const roleTitle = roleRes[0].title;
+          result[0].role = roleTitle;
+          const token = jwt.sign(
+            { id: result[0].id },
+            "the-super-strong-secrect",
+            { expiresIn: "1h" }
+          );
+          connection.query(
+            `UPDATE users SET created_date = now() WHERE id = '${result[0].id}'`,
+            (updateErr, updateRes) => {
+              if (updateErr) throw updateErr;
+              return res.status(200).send({
                 msg: "Logged in!",
                 token,
                 user: result[0],
-            });
+              });
             }
-          return res.status(401).send({
-              msg: "Username or password is incorrect!",
-          });
-        })
-    })
+          );
+        }
+      );
+    });
+  });
 });
-  
+
 //signupValidation
 router.post("/api/get-user", (req, res, next) => {
-    if (
-      !req.headers.authorization ||
-      !req.headers.authorization.startsWith("Bearer") ||
-      !req.headers.authorization.split(" ")[1]
-    ) {
-      return res.status(422).json({
-        message: "Please provide the token",
-      });
-    }
-    const theToken = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(theToken, "the-super-strong-secrect");
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer") ||
+    !req.headers.authorization.split(" ")[1]
+  ) {
+    return res.status(422).json({
+      message: "Please provide the token",
+    });
+  }
+  const theToken = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(theToken, "the-super-strong-secrect");
 
-    connection.query(
-      "SELECT * FROM users where id = ?",
-      decoded.id,
-      function (error, results, fields) {
-        if (error) throw error;
-        connection.query("SELECT * FROM roles WHERE id = ?", results[0].role_id, (roleErr, roleRes) => {
+  connection.query(
+    "SELECT * FROM users where id = ?",
+    decoded.id,
+    function (error, results, fields) {
+      if (error) throw error;
+      connection.query(
+        "SELECT * FROM roles WHERE id = ?",
+        results[0].role_id,
+        (roleErr, roleRes) => {
           if (roleErr) throw roleErr;
           const roleTitle = roleRes[0].title;
           results[0].role = roleTitle;
@@ -118,9 +136,10 @@ router.post("/api/get-user", (req, res, next) => {
             data: results[0],
             message: "Fetch Successfully.",
           });
-        })
-      }
-    );
-  });
+        }
+      );
+    }
+  );
+});
 
 module.exports = router;
