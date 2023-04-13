a4eb3f6518a912d7b198244664c584fa303de0aa
YuukiTS01 <lenamthaisonts@gmail.com>
2023-04-13 12:29:53 +0700

    change error handling

--- routes/area.js
+++ routes/area.js
@@ -5,7 +5,7 @@ var connection = require('../models/dbconfig')
 router.get('/province', function(req, res) {
     var query = "select id, name from province";
     connection.query(query, function(err, result) {
-        if (err) throw err;
+        if (err) return res.send(err);
         res.send(result);
     });
 });
@@ -14,7 +14,7 @@ router.get('/province/:provinceid', function(req, res) {
     var query = "select id, name from province where id = ?";
     var params = [req.params.provinceid];
     connection.query(query, params, function(err, result) {
-        if(err) throw err;
+        if(err) return res.send(err);
         res.send(result);
     });
 });
@@ -23,7 +23,7 @@ router.get('/province/:provinceid/district', function(req, res) {
     var query = "call sp_dist_by_province(?)";
     var params = [req.params.provinceid];
     connection.query(query, params, function(err, result) {
-        if(err) throw err;
+        if(err) return res.send(err);
         res.send(result);
     });
 });
@@ -32,7 +32,7 @@ router.get('/district/:districtid', function(req, res) {
     var query = "select id, title from district where id = ?";
     var params = req.params.districtid;
     connection.query(query, params, function(err, result) {
-        if(err) throw err;
+        if(err) return res.send(err);
         res.send(result);
     });
 });
@@ -41,7 +41,7 @@ router.get('/district/:districtid/ward', function(req, res) {
     var query = "call sp_ward_by_dist(?)";
     var params = req.params.districtid;
     connection.query(query, params, function(err, result) {
-        if(err) throw err;
+        if(err) return res.send(err);
         res.send(result);
     });
 });
@@ -50,7 +50,7 @@ router.get('/ward/:wardid', function(req, res) {
     var query = "select id, title from ward where id = ?";
     var params = req.params.wardid;
     connection.query(query, params, function(err, result) {
-        if(err) throw err;
+        if(err) return res.send(err);
         res.send(result);
     });
 });
